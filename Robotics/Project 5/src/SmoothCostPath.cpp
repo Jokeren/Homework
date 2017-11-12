e3c9caeb0895a68f99d0ce83c9cbd76f72a2bcf4
jokeren <robinho364@gmail.com>
2017-11-12 13:29:04 -0600

    Init smoothess

--- Robotics/Project 5/src/SmoothCostPath.cpp
+++ Robotics/Project 5/src/SmoothCostPath.cpp
@@ -6,6 +6,7 @@
 //////////////////////////////////////
 
 #include "SmoothCostPath.h"
+#include <cmath>
 
 namespace ompl
 {
@@ -19,7 +20,20 @@ namespace ompl
             }
             else 
             {
-                return 0;
+                std::vector<base::State *> &states = path_.getStates();
+                double a = path_.getSpaceInformation()->distance(states[index - 1], state);
+                double b = path_.getSpaceInformation()->distance(state, states[index + 1]);
+                double c = path_.getSpaceInformation()->distance(states[index - 1], states[index + 1]);
+                double acosValue = (a * a + b * b - c * c) / (2.0 * a * b);
+            	if (acosValue > -1.0 && acosValue < 1.0)
+				{
+					// the smoothness is actually the outside angle of the one we compute
+					double angle = (M_PI - acos(acosValue));
+					// and we normalize by the length of the segments
+					double k = 2.0 * angle / (a + b);
+					return k * k;
+				}
+				return 0;
             }
         }
     }
