ebdd11fd363f8ea290e912dab0ec743f969b1544
Jokeren <robinho364@gmail.com>
2018-02-11 22:21:35 -0600

    RM old files

--- Determinant/kernels/funcs.h
+++
@@ -1,8 +0,0 @@
-#ifndef _DETERMINANT_KERNELS_FUNCS_H_
-#define _DETERMINANT_KERNELS_FUNCS_H_
-
-typedef float (*determinant_s_fn_t)(float *M);
-
-extern *determinant_s_fn_t lookup_determinant_s(char *kernel_name);
-
-#endif  // _DETERMINANT_KERNELS_KERNEL_H_
