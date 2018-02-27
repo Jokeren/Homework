4829b4a3d9c93229e6ef8f40cba6dec65c44d70a
Jokeren <robinho364@gmail.com>
2018-02-27 15:15:55 -0600

    Add benchmark mode

--- Determinant/determinant.c
+++ Determinant/determinant.c
@@ -1,10 +1,13 @@
 // Local include
 #include <determinant.h>
+#include <utils/timer.h>
 // System include
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 
+#define BENCH_TIMES 100
 
 static void get_matrix_s(const char *file_name, size_t *n, float **m) {
   FILE *fp;
@@ -67,7 +70,7 @@ static void get_matrix_ll(const char *file_name, size_t *n, long long **m) {
 
 
 int main(int argc, char *argv[]) {
-  if (argc != 4) {
+  if (argc < 4 || argc > 6) {
     fprintf(stderr, "Arguments error!\n");
     fprintf(stderr, "arg[1]: file_name!\n");
     fprintf(stderr, "arg[2]: data_type!\n");
@@ -79,13 +82,47 @@ int main(int argc, char *argv[]) {
   char *file_name = argv[1];
   char *data_type = argv[2];
   char *kernel_name = argv[3];
+  bool bench = false;
+  size_t times = 1;
+
+  if (argc > 4) {
+    char *bench_str = argv[4];
+    if (strcmp(bench_str, "bench") == 0) {
+      bench = true;
+    } else {
+      exit(1);
+    }
+    if (argc == 5) {
+      times = BENCH_TIMES;
+    } else {
+      times = atoi(argv[5]);
+    }
+  }
 
   if (strcmp(data_type, "float") == 0) {
     float *m;
     determinant_s_fn_t determinant_s_fn = lookup_determinant_func(data_type, kernel_name);
     if (determinant_s_fn != NULL) {
       get_matrix_s(file_name, &n, &m);
-      float result = determinant_s_fn(n, m);
+      float result = 0.0f;
+      if (bench == true) {
+        float elapsed;
+        struct timeval t1, t2;
+        float *m_array = (float *)malloc(sizeof(float) * times * n * n);
+        size_t i;
+        for (i = 0; i < times; ++i) {
+          memcpy(m_array + i * n * n, m, sizeof(float) * n * n);
+        }
+        CPU_TIMER_START(elapsed, t1);
+        for (i = 0; i < times; ++i) {
+          result = determinant_s_fn(n, m_array + i * n * n);
+        }
+        CPU_TIMER_END(elapsed, t1, t2);
+        printf("%fs\n", elapsed);
+        free(m_array);
+      } else {
+        result = determinant_s_fn(n, m);
+      }
       printf("%f\n", result);
     }
     free(m);
@@ -94,8 +131,26 @@ int main(int argc, char *argv[]) {
     determinant_d_fn_t determinant_d_fn = lookup_determinant_func(data_type, kernel_name);
     if (determinant_d_fn != NULL) {
       get_matrix_d(file_name, &n, &m);
-      double result = determinant_d_fn(n, m);
-      printf("%lf\n", result);
+      float result = 0.0f;
+      if (bench == true) {
+        double elapsed;
+        struct timeval t1, t2;
+        double *m_array = (double *)malloc(sizeof(double) * times * n * n);
+        size_t i;
+        for (i = 0; i < times; ++i) {
+          memcpy(m_array + i * n * n, m, sizeof(double) * n * n);
+        }
+        CPU_TIMER_START(elapsed, t1);
+        for (i = 0; i < times; ++i) {
+          result = determinant_d_fn(n, m_array + i * n * n);
+        }
+        CPU_TIMER_END(elapsed, t1, t2);
+        printf("%fs\n", elapsed);
+        free(m_array);
+      } else {
+        result = determinant_d_fn(n, m);
+      }
+      printf("%f\n", result);
     }
     free(m);
   } else if (strcmp(data_type, "ll") == 0) {
@@ -103,7 +158,25 @@ int main(int argc, char *argv[]) {
     determinant_ll_fn_t determinant_ll_fn = lookup_determinant_func("long long", kernel_name);
     if (determinant_ll_fn != NULL) {
       get_matrix_ll(file_name, &n, &m);
-      long long result = determinant_ll_fn(n, m);
+      long long result = 0;
+      if (bench == true) {
+        double elapsed;
+        struct timeval t1, t2;
+        long long *m_array = (long long *)malloc(sizeof(long long) * times * n * n);
+        size_t i;
+        for (i = 0; i < times; ++i) {
+          memcpy(m_array + i * n * n, m, sizeof(long long) * n * n);
+        }
+        CPU_TIMER_START(elapsed, t1);
+        for (i = 0; i < times; ++i) {
+          result = determinant_ll_fn(n, m_array + i * n * n);
+        }
+        CPU_TIMER_END(elapsed, t1, t2);
+        printf("%fs\n", elapsed);
+        free(m_array);
+      } else {
+        result = determinant_ll_fn(n, m);
+      }
       printf("%lld\n", result);
     }
     free(m);
