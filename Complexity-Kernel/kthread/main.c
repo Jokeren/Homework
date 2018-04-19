09f42c4c47ec117fb8977b1b8160e66f2471ac6f
jokeren <robinho364@gmail.com>
2018-04-18 22:41:30 -0500

    Add locks

--- Complexity-Kernel/kthread/main.c
+++ Complexity-Kernel/kthread/main.c
@@ -1,14 +1,21 @@
-#include<linux/init.h>
-#include<linux/module.h>
-#include<linux/kernel.h>
-#include<linux/kthread.h>
-#include<linux/sched.h>
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/semaphore.h>
+#include <linux/kthread.h>  // for threads
+#include <linux/sched.h>  // for task_struct
+#include <linux/time.h>
+#include <linux/timer.h>
+
 
 struct task_struct *reader_task;
 struct task_struct *writer_task;
 struct task_struct *compute_task;
 int reader_id, writer_id, compute_id;
-
+DEFINE_SPINLOCK(compute_read_lock);
+DEFINE_SPINLOCK(compute_write_lock);
+DEFINE_SPINLOCK(writer_read_lock);
+DEFINE_SPINLOCK(writer_write_lock);
 
 static int reader_function(void *data) {
   int count;
@@ -16,7 +23,9 @@ static int reader_function(void *data) {
   int i;
   i = 0;
   while (i < count) {
+    spin_lock(&compute_write_lock);
     printk(KERN_INFO"In Kernel Reader Thread %d\n", i);
+    spin_unlock(&compute_read_lock);
     ++i;
   }
   return 0;
@@ -29,7 +38,9 @@ static int writer_function(void *data) {
   int i;
   i = 0;
   while (i < count) {
+    spin_lock(&writer_read_lock);
     printk(KERN_INFO"In Kernel Writer Thread %d\n", i);
+    spin_unlock(&writer_write_lock);
     ++i;
   }
   return 0;
@@ -42,7 +53,11 @@ static int compute_function(void *data) {
   int i;
   i = 0;
   while (i < count) {
+    spin_lock(&compute_read_lock);
     printk(KERN_INFO"In Kernel Compute Thread %d\n", i);
+    spin_unlock(&compute_write_lock);
+    spin_lock(&writer_write_lock);
+    spin_unlock(&writer_read_lock);
     ++i;
   }
   return 0;
@@ -54,6 +69,8 @@ static int kernel_init(void) {
   reader_id = 1;
   writer_id = 2;
   compute_id = 3;
+  spin_lock(&compute_read_lock);
+  spin_lock(&writer_read_lock);
   reader_task = kthread_run(&reader_function, (void *)&reader_id, "reader_task");
   writer_task = kthread_run(&writer_function, (void *)&writer_id, "writer_task");
   compute_task = kthread_run(&compute_function, (void *)&compute_id, "compute_task");
